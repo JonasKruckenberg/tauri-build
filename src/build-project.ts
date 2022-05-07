116ed3b1401dce75f205c573c095225b884c6fcd
Jonas Kruckenberg <iterpre@protonmail.com>
2022-05-07 12:45:53 +0200

    add logic

---
+++ src/build-project.ts
@@ -0,0 +1,43 @@
+import {execa} from 'execa'
+import {join} from 'path'
+import glob from 'tiny-glob'
+
+interface BuildOptions {
+  runner?: string
+  projectPath?: string
+  configPath?: string
+  args?: string[]
+  target?: string
+}
+
+export async function buildProject(options: BuildOptions): Promise<string[]> {
+  const projectPath = options.configPath || process.cwd()
+  const runner = options.runner || 'tauri'
+  let args: string[] = options.args || []
+
+  if (options.configPath) {
+    args.push('--config', options.configPath)
+  }
+
+  if (options.target) {
+    args.push('--target', options.target)
+  }
+
+  await execa(runner, args, {cwd: projectPath})
+
+  const outDir = options.target
+    ? `./target/${options.target}/release/bundle`
+    : `./target/release/bundle`
+  const macOSExts = ['app', 'app.tar.gz', 'app.tar.gz.sig', 'dmg']
+  const linuxExts = [
+    'AppImage',
+    'AppImage.tar.gz',
+    'AppImage.tar.gz.sig',
+    'deb'
+  ]
+  const windowsExts = ['msi', 'msi.zip', 'msi.zip.sig']
+
+  return glob(
+    join(outDir, `*/*.{${[...macOSExts, linuxExts, windowsExts].join(',')}}`)
+  )
+}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {buildProject} from './build-project'
+import stringArgv from 'string-argv';
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const artifacts = await buildProject({
+      runner: core.getInput('runner'),
+      args: stringArgv(core.getInput('args')),
+      projectPath: core.getInput('projectPath'),
+      configPath: core.getInput('configPath'),
+      target: core.getInput('target')
+    })
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('artifacts', artifacts.join('\n'))
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
